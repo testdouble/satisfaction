36ad2a5ef0bd1be8b3260471e7d001768f2ae480
Micah Adams <micah.shawn.adams@gmail.com>
2018-11-07 10:53:20 -0700

    setting up project

---
+++ spec/satisfaction_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Satisfaction do
+  it "has a version number" do
+    expect(Satisfaction::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
